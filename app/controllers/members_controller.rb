917fe05d9d3071b52a8c737c963f478d1b145995
Kento Yamamoto <key7135g@gmail.com>
2024-11-23 00:03:49 +0900

    memberテーブルを追加

---
+++ app/controllers/members_controller.rb
@@ -0,0 +1,29 @@
+class MembersController < ActionController::Base
+  layout "application"
+  before_action :correct_room
+
+  def correct_room
+    if current_room
+      unless current_room.uuid == params['id']
+        redirect_to('/rooms/login_form')
+      end
+    else
+      redirect_to('/rooms/login_form')
+    end
+  end
+
+  def current_room
+    Room.find_by(uuid: session[:room_id])
+  end
+
+  def new
+    @room = Room.find_by(uuid: params['id'])
+  end
+
+  def create
+    @room = Room.find_by(uuid: params['id'])
+    @member = Member.new(room_id: @room.uuid, name: params['name'])
+    redirect_to("/rooms/#{@room.uuid}")
+  end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_11_20_071436) do
+ActiveRecord::Schema[7.0].define(version: 2024_11_22_135437) do
+  create_table "members", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "room_id", null: false
+    t.string "name"
+    t.boolean "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["room_id"], name: "index_members_on_room_id"
+  end
+
   create_table "rooms", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "uuid"
     t.string "name"
@@ -19,4 +28,5 @@ ActiveRecord::Schema[7.0].define(version: 2024_11_20_071436) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "members", "rooms"
 end
